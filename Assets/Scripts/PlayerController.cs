6cdec25d3913c7d57b912ba9a99c61f4b5a0727d
Cassia <40779641+FredericaErath@users.noreply.github.com>
2024-02-02 15:27:30 -0800

    add different beat to different platforms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Ground1"))
         {
             isGrounded = true;
         }
