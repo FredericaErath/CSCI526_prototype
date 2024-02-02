4ad33ba260f77a769c40ddb307eb7954cb77dec8
Khushi Goyal <khushi.goyal25@gmail.com>
2024-02-02 14:10:02 -0800

    Music updown

---
+++ Assets/TriangleMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2.0f; // Adjust this value to set the movement speed
+    public float amplitude = 1.0f; // Adjust this value to set the movement amplitude
+
+    private float originalY;
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                // Calculate the vertical movement using Mathf.Sin for a smooth up-and-down motion
+        float verticalMovement = amplitude * Mathf.Sin(Time.time * speed);
+
+        // Set the new position of the GameObject
+        transform.position = new Vector3(transform.position.x, originalY + verticalMovement, transform.position.z);
+
+    }
+}
