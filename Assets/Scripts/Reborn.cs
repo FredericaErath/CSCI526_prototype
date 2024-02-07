a2b562279b8a18f69257c532a584d0fc392d4b16
Cassia <40779641+FredericaErath@users.noreply.github.com>
2024-02-06 16:19:03 -0800

    bug fix

--- Assets/Scripts/Reborn.cs
+++ Assets/Scripts/Reborn.cs
@@ -21,10 +21,9 @@ public class Reborn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb.position.y < -6f) // when the player fall out of the scene
+        if (rb.position.y < -4f) // when the player fall out of the scene
         {
             Respawn();
-            
         }
     }
 }
